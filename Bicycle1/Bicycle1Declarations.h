4826539e985f1d8e8f0d5573da90fd398363a25d
Matthew Walker <mattwalker101@hotmail.com>
2014-09-23 20:37:47 -0700

    Create Bicycle1Declarations.h

---
+++ Bicycle1/Bicycle1Declarations.h
@@ -0,0 +1,13 @@
+// Bicycle1Declarations.h
+// all variables declared here
+
+int speed{};
+const int MINspeed{ 10 };
+// const int MINspeed;
+const int MAXspeed{ 40 };
+
+int GetSpeed();
+int GetMinSpeed();
+int GetMaxSpeed();
+void SetSpeed();
+void SetSpeed(int);
