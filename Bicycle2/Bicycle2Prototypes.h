26c7273b982229db383e4af70eaab186debaab67
Matthew Walker <mattwalker101@hotmail.com>
2014-09-30 20:40:37 -0700

    Create Bicycle2Prototypes.h

---
+++ Bicycle2/Bicycle2Prototypes.h
@@ -0,0 +1,11 @@
+// Bicycle2Prototypes.h
+
+
+int GetSpeed();
+int GetMinSpeed();
+int GetMaxSpeed();
+void SetSpeed();
+void SetSpeed(int);
+void DefaultSetSpeed(int s = 20);
+void Swap(int &, int &);
+void DistanceTravelled(int);
