b223ecd5bff469758ef3b37b10558de05f3e0891
Matthew Walker <mattwalker101@hotmail.com>
2014-10-16 19:49:35 -0700

    Create Bicycle2Prototypes.h

---
+++ Lab6_Bicycle2_Part3/Bicycle2Prototypes.h
@@ -0,0 +1,19 @@
+// Bicycle2Prototypes.h
+
+
+int GetSpeed();
+int GetMinSpeed();
+int GetMaxSpeed();
+void SetSpeed();
+void SetSpeed(int);
+void DefaultSetSpeed(int s = 20);
+void Swap(int &, int &);
+void DistanceTravelled(int);
+void GetSelectedSpeed();
+void DisplayMenu();
+ void ImplementSelection();
+int MenuSelection();
+// void MenuSelection();
+void GetSelection();
+bool CheckContinue();
+void SelectModel();
