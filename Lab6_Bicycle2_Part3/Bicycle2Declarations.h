072fc313d4011d9e4504a76c6ef85257bf580b64
Matthew Walker <mattwalker101@hotmail.com>
2014-10-16 19:49:10 -0700

    Create Bicycle2Declarations.h

---
+++ Lab6_Bicycle2_Part3/Bicycle2Declarations.h
@@ -0,0 +1,10 @@
+// Bicycle2Declarations.h
+// all variables declared here
+
+int speed{};	// default = 0
+const int MINspeed{ 10 };
+const int MAXspeed{ 40 };
+
+int selection{};
+
+enum BicycleModels { Road=1, Mountain, Racing, Folding, Electric };
