9a9dd0f983c31d315ad5b2737404b5312a3b3aec
Matthew Walker <mattwalker101@hotmail.com>
2014-10-28 20:34:18 -0700

    Create Shapes.h

---
+++ Shapes2/Shapes.h
@@ -0,0 +1,30 @@
+// Shapes.h
+// class declarations
+
+class Shapes {
+private:
+	char selection;
+
+	void DisplayMenu();
+	void GetSelection();
+	void ImplementSelection();
+	void Rectangle();
+	void Triangle();
+	void Trapezoid();
+	void Circle();
+	double GetLength();
+
+public:
+	Shapes(); 
+
+	char GetCurrentSelection();
+	//void MenuSelection();
+	//void DisplayMenu();
+	//void GetSelection();
+	//void ImplementSelection();
+	char MenuSelection();
+	bool CheckContinue();
+	static double rArea, tArea, zArea, cArea;
+	
+};
+
