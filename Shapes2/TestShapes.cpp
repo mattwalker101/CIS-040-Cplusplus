cd537193cd7130898f63c8094552aca8aeef86c7
Matthew Walker <mattwalker101@hotmail.com>
2014-10-28 20:35:31 -0700

    Create TestShapes.cpp

---
+++ Shapes2/TestShapes.cpp
@@ -0,0 +1,76 @@
+
+
+/********************************************************************************
+*
+*      Mission College C++ Class
+*
+********************************************************************************
+*
+*      Author: Matthew Walker
+
+*      Program Name: TestShapes.cpp
+*
+*      Date: 10/21/14
+*
+*      Purpose: To calculate the area of some shapes
+*
+*
+*
+*********************************************************************************/
+
+#include "LibIncludes.h"
+#include "Shapes.h"
+
+int main() {
+
+	Shapes shape;
+	cout << "\"selection\" = " << static_cast<int>(shape.GetCurrentSelection()) << endl;
+
+//	shape.DisplayMenu();
+//	shape.MenuSelection();
+
+	
+/*	char menuSelection = shape.MenuSelection();
+	cout << "selection in main() = " << menuSelection << endl;
+
+	if (menuSelection != 'X') {
+		bool status = shape.CheckContinue();
+		cout << "\nstatus = " << boolalpha
+			<< status << "\n\n";
+	}
+	else {
+		cout << "\nGoodbye!\n\n";
+	}
+*/
+	
+	while (true) {
+		char menuSelection = shape.MenuSelection();
+
+		if (menuSelection != 'X') {
+			if (shape.CheckContinue()) {
+				system("cls");
+				continue;
+			}
+			else {
+				cout << "\nRectangle area = " << Shapes::rArea << endl;
+				cout << "Triangle area = " << Shapes::tArea << endl;
+				cout << "Trapezoid area = " << Shapes::zArea << endl;
+				cout << "Circle area = " << Shapes::cArea << endl;
+
+				cout << "\nGoodbye!\n\n";
+				break;
+			}
+			// cout << "\nstatus = " << boolalpha
+			//	<< status << "\n\n";
+		}
+		else {
+			cout << "\nRectangle area = " << Shapes::rArea << endl;
+			cout << "Triangle area = " << Shapes::tArea << endl;
+			cout << "Trapezoid area = " << Shapes::zArea << endl;
+			cout << "Circle area = " << Shapes::cArea << endl;
+
+			cout << "\nGoodbye!\n\n";
+			break;
+		}
+	}
+}
