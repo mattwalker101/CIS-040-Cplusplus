4b0a17499bf85d9c303bcedd5fbb56e761e8d299
Matthew Walker <mattwalker101@hotmail.com>
2014-12-02 21:05:18 -0800

    Create TestArray.cpp

---
+++ ProcessArray/TestArray.cpp
@@ -0,0 +1,29 @@
+/********************************************************************************
+*
+*      Mission College C++ Class
+*
+********************************************************************************
+*
+*      Author: Matthew Walker
+
+*      Program Name: TestArray.cpp
+*
+*      Date: 12/4/14
+*
+*      Purpose: 
+*
+*
+*
+*********************************************************************************/
+
+
+#include <iostream>
+#include "ProcessArray.h"
+
+int main() {
+
+	ProcessArray obj = ProcessArray();
+	obj.UsingCTypeAlgorithms();
+	obj.UsingCppTypeAlgorithms();
+
+}
