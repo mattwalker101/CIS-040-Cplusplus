900c5d5f87fef91896ed19f964ca9bc5cf8261a8
Matthew Walker <mattwalker101@hotmail.com>
2014-12-04 19:52:48 -0800

    Update ProcessCPPArray.cpp

--- ProcessArray/ProcessCPPArray.cpp
+++ ProcessArray/ProcessCPPArray.cpp
@@ -10,6 +10,8 @@ bool Greater60(int i) {
 }
 
 void ProcessArray::UsingCppTypeAlgorithms() {
+
+	cout << "*** C++ TYPE ARRAY ***";
 	// display size of cppArray
 	cout << "\nSize of cppArray is: " << cppArray.size() << endl;
 
@@ -22,13 +24,13 @@ void ProcessArray::UsingCppTypeAlgorithms() {
 	cout << "\n\n";
 
 	// re-initialize in range 0-100 and re-display
-	for (auto i = cppArray.begin(); i != cppArray.end; i++) {
+	for (auto i = cppArray.begin(); i != cppArray.end(); i++) {
 		*i = rand() % 100;
 		cout << *i << " ";
 	}
 
 	// use accumulate() and display sum of elements
-	cout << "\nSum of elements is: " << accumulate(cppArray.begin(), cppArray.end());
+	cout << "\nSum of elements is: " << accumulate(begin(cppArray), end(cppArray), 0) << endl;
 
 	// find max and min value
 	auto maxValue = max_element(cppArray.begin(), cppArray.end());
@@ -49,7 +51,7 @@ void ProcessArray::UsingCppTypeAlgorithms() {
 		cout << first << " not found!\n\n";
 
 	first = { 42 };
-	auto ptr = find(cppArray.begin(), cppArray.end(), first);
+	ptr = find(cppArray.begin(), cppArray.end(), first);
 
 	if (ptr < cppArray.end())
 		cout << "Value " << first << " was found at index "
@@ -62,16 +64,98 @@ void ProcessArray::UsingCppTypeAlgorithms() {
 	int n = count(cppArray.begin(), cppArray.end(), cppArray[0]);
 	cout << "There are " << n << " values of " << cppArray[0] << " found!\n\n";
 
-	//
+	// use countif() to return number of values > 60
+	int m = count_if(cppArray.begin(), cppArray.end(), Greater60);
+	cout << "There are " << m << " values greater than 60 in cppArray.\n";
+
+	// use is_sorted()
 	bool b = is_sorted(begin(cppArray), end(cppArray));
+	if (b == true)
+		cout << "cppArray is sorted.\n";
+	else
+		cout << "cppArray is not sorted.\n"; 
+
+	// use sort()
+	sort(cppArray.begin(), cppArray.end());
+	cout << "cppArray has been sorted!\n";
+	for (auto i : cppArray)
+		cout << i << " ";
+	cout << "\n\n";
+
+	// use is_sorted again
+	b = is_sorted(begin(cppArray), end(cppArray));
+	if (b == true)
+		cout << "cppArray is sorted.\n";
+	else
+		cout << "cppArray is not sorted.\n";
+
+	cout << "Reversing the sort: ";
+	sort(cppArray.begin(), cppArray.end(), greater<int>());
+	for (auto i : cppArray)
+		cout << i << " ";
+	cout << "\n\n";
+	
+	cout << "Reverse the sort again: ";
+	reverse(cppArray.begin(), cppArray.end());
+	for (auto i : cppArray)
+		cout << i << " ";
+	cout << "\n\n";
+	
+	// use binary_search()
+	cout << "Using binary_search()\n";
+	int key{ 53 };
+	b = binary_search(cppArray.begin(), cppArray.end(), key);
+
+	if (b == true)
+		cout << "Element " << key << " found.\n";
+	else
+		cout << "Element " << key << " not found.\n";
+
+	// using random shuffle
+	cout << "Using random_shuffle\n";
+	random_shuffle(cppArray.begin(), cppArray.end());
+	for (auto i : cppArray)
+		cout << i << " ";
+	cout << "\n\n";
+
+	// using LinearSearch
+	key = { 34 };
+	int element = LinearSearchCppArray(cppArray, key);
+
+	if (element != -1)
+		cout << "Number " << key << " found at index " << element << endl;
+	else
+		cout << "Number " << key << " not found.\n";
+
+	// using SelectionSort to reverse order
+	SelectionSortCppArray(cppArray);
+	cout << "cppArray in descending order: \n";
+	for (auto i : cppArray)
+		cout << i << " ";
+	cout << "\n\n";
 
 };
 
-int ProcessArray::LinearSearchCppArray(array<int, SIZE>arr, key) {
+int ProcessArray::LinearSearchCppArray(array<int, SIZE>arr, int key) {
 	for (int j : arr)
 		if (j == key)
 			return j;
 	return -1;
 }
 
-void ProcessArray::SelectionSortCppArray(array<int, SIZE> &)
+void ProcessArray::SelectionSortCppArray(array<int, SIZE> &arr) {
+	int highest;
+
+	for (size_t i = 0; i < SIZE - 1; i++) {
+		highest = i;
+
+		for (size_t j = i + 1; j < SIZE; j++)
+			if (arr[j] > arr[highest])
+				highest = j;
+		// swap
+		int temp;
+		temp = arr[highest];
+		arr[highest] = arr[i];
+		arr[i] = temp;
+	}
+}
